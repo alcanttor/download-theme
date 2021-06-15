e6b31d3b3cefb65aeca23e209651f04598c9da55
mJawed <admin@localhost>
2021-06-15 16:41:52 +0530

    Added popup

--- download-theme.php
+++ download-theme.php
@@ -4,7 +4,7 @@ Plugin Name: Download Theme
 Plugin URI: http://metagauss.com
 Description: Download any theme from your wordpress admin panel's Appearance page by just one click!
 Version: 1.0.7
-Author: metagauss
+Author: Metagauss
 Author URI: https://profiles.wordpress.org/metagauss/
 Text Domain: download-theme
 */
@@ -119,3 +119,48 @@ function dtwap_download(){
 }
 add_action( 'admin_init', 'dtwap_download' );
 ?>
+
+<div class="dtwap-modal-view">
+    
+<div class="dtwap-modal-overlay dtwap-popup-overlay-fade-in"></div>
+
+<div class="dtwap-modal-wrap dtwap-popup-out">
+       <button class="dtwap-modal-close dtwap-close-btn " type="button" >&#10005;</button>
+    <div class="dtwap_section_first-slide dpwap_section_content current">
+        <div class="dtwap_row dtwap__head_row">
+             <div class="dpwap-head-img">
+                        <img src="<?php echo plugin_dir_url( __FILE__ ) . 'images/slide-1.png'; ?>">
+                    </div>
+            
+        </div> 
+        
+        <div class="dtwap__head_body">
+
+            <div class="dtwap_row">
+                <div class="dtwap_slide_head dtwap-section-col">
+                    Thank you for installing Download theme!
+                </div>
+            </div>
+
+            <div class="dtwap_row">
+                <div class="dtwap-section-col">
+          It is now awfully easy to download any theme from your website dashboard with a single click!
+                </div>
+            </div>
+            
+            <div class="dtwap_modal_footer">
+
+                <button class="btn btn-default next-tab dtwap-modal-close" id="second-slide-button" type="button">Close</button>
+            </div>
+
+
+        </div>
+        
+    </div>
+    
+</div>
+
+</div>
+
+
+
--- js/dtwap-admin.js
+++ js/dtwap-admin.js
@@ -8,3 +8,39 @@ jQuery(document).ready( function($){
 		$(this).prepend(download_link)
 	});
 });
+
+
+// Plugin Info Modal
+
+
+jQuery(document).ready(function(){
+  jQuery('.dtwap-modal-view').addClass('is-active');
+    jQuery('.dtwap-modal-wrap').removeClass('dtwap-popup-out');
+    jQuery('.dtwap-modal-wrap').addClass('dtwap-popup-in');
+    jQuery('.dtwap-modal-overlay').removeClass('dtwap-popup-overlay-fade-out');
+    jQuery('.dtwap-modal-overlay').addClass('dtwap-popup-overlay-fade-in');
+  
+});
+
+
+jQuery(document).ready(function () {
+    jQuery('.dtwap-modal-close, .dtwap-modal-overlay').click(function () {
+        setTimeout(function () {
+            //jQuery(this).parents('.rm-modal-view').hide();
+            jQuery('.dtwap-modal-view').hide();
+        }, 400);
+    });
+    jQuery('.dtwap-modal-close, .dtwap-modal-overlay').on('click', function () {
+        jQuery('.dtwap-modal-wrap').removeClass('dtwap-popup-in');
+        jQuery('.dtwap-modal-wrap').addClass('dtwap-popup-out');
+
+        jQuery('.dtwap-modal-overlay').removeClass('dtwap-popup-overlay-fade-in');
+        jQuery('.dtwap-modal-overlay').addClass('dtwap-popup-overlay-fade-out');
+    });
+
+
+
+
+
+
+});
