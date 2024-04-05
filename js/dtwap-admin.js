1e539801fb72ec174d6fa01bf6fcba447394b300
mJawed <mohd.jawed@alcanttor.com>
2024-04-05 14:27:30 +0530

    Minor alignment fixes

--- js/dtwap-admin.js
+++ js/dtwap-admin.js
@@ -2,7 +2,7 @@ jQuery(document).ready( function($){
 	if ( $('.themes .theme .theme-actions').length > 0 ){
 		$('.themes .theme .theme-actions').each( function( index, element ){
 			var theme_folder  = $(this).parents('.theme').attr('data-slug');
-			var download_link = '<a href="?dtwap_download='+theme_folder+'&_wpnonce='+dtwap.dtwap_nonce+'" class="button button-primary dtwap_download_link dashicons dashicons-download" style="font-weight:700; width:auto;">'+dtwap.download_title+'</a>';
+			var download_link = '<a href="?dtwap_download='+theme_folder+'&_wpnonce='+dtwap.dtwap_nonce+'" class="dp-download-button-custom button button-primary dtwap_download_link" style="font-weight:700; width:auto;"><span class="dashicons dashicons-download"></span>' +dtwap.download_title+'</a>';
 			$(this).prepend(download_link);
 		});
 	}
